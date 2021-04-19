b558ab06e3a1b43e7a27e66a83c48b1b355bf088
DeadScreams <alexsator.lukin@gmail.com>
2021-04-19 21:16:17 +0300

    Update db.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const path = require('path')
 
+import "./db.js"
+
 const app = express()
 const router = express.Router()
 const port = 3000
@@ -12,6 +14,8 @@ router.get('/api/tasks', (req,res) => {
     res.send(JSON.stringify({tasks : tasks}))
     console.log(req.query.id)
 })
+
+
 
 app.use('/', router)
 app.listen(port, hostname, () => console.log("Server Up"))
